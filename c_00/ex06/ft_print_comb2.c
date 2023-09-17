97496b4d880ddc9b956f4b1845b94ad7cee8fbba
Zakariae Mourid <zmourid@e2r11p10.1337.ma>
2023-09-17 21:10:04 +0100

    asda

--- c_00/ex06/ft_print_comb2.c
+++ c_00/ex06/ft_print_comb2.c
@@ -1,58 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb2_test_clean.c                        :+:      :+:    :+:   */
+/*   ft_print_comb3.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zmourid <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/13 22:26:17 by zmourid           #+#    #+#             */
-/*   Updated: 2023/09/15 20:58:31 by zmourid          ###   ########.fr       */
+/*   Created: 2023/09/17 20:11:17 by zmourid           #+#    #+#             */
+/*   Updated: 2023/09/17 20:58:37 by zmourid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	ft_put_nbrs(int i, int j, int k, int l)
+void	ft_putchar(char nbr)
 {
-	write(1, &i, 1);
-	write(1, &j, 1);
-	write(1, " ", 1);
-	write(1, &k, 1);
-	write(1, &l, 1);
-	if (!(i == 57 && j == 56 && k == 57 && l == 57))
-		write(1, ", ", 2);
-	l++;
-	return (l);
+	write(1, &nbr, 1);
 }
 
 void	ft_print_comb2(void)
 {
-	int	i;
-	int	j;
-	int	k;
-	int	l;
+	int	a;
+	int	b;
 
-	i = 48;
-	while (i <= 57)
+	a = 0;
+	while (a <= 98)
 	{
-	j = 48;
-		while (j <= 57)
+		b = a +1;
+		while (b <= 99)
 		{
-			k = i;
-			while (k <= 57)
-			{
-				l = j + 1;
-				while (l <= 57)
-				{
-					l = ft_put_nbrs(i, j, k, l);
-				}
-				k++;
-			}
-			j++;
+			ft_putchar('0' + (a / 10));
+			ft_putchar('0' + (a % 10));
+			write(1, " ", 1);
+			ft_putchar('0' + (b / 10));
+			ft_putchar('0' + (b % 10));
+			if (a != 98)
+				write(1, ", ", 2);
+			b++;
 		}
-		i++;
+		a++;
 	}
-}
-int main(void){
-	ft_print_comb2();
 }
