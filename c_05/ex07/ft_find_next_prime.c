b8a6a8351b446106261ca1d3e19ba2e5b71c2411
Zakariae Mourid <zmourid@e2r7p2.1337.ma>
2023-09-26 20:56:20 +0100

    lesgo2

--- c_05/ex07/ft_find_next_prime.c
+++ c_05/ex07/ft_find_next_prime.c
@@ -6,47 +6,39 @@
 /*   By: zmourid <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 15:03:22 by zmourid           #+#    #+#             */
-/*   Updated: 2023/09/26 20:47:32 by zmourid          ###   ########.fr       */
+/*   Updated: 2023/09/26 20:56:10 by zmourid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_is_prime(int nb)
 {
-	int		count;
 	long	i;
 	long	nbr;
 
 	i = 2;
 	nbr = nb;
-	count = 0;
 	if (nbr <= 1)
 		return (0);
 	if (nbr <= 3)
 		return (1);
-	while (i < nbr / i)
+	while (i <= nbr / i)
 	{
 		if (nbr % i == 0)
-			count++;
+			return (0);
 		i++;
 	}
-	if (count >= 2)
-		return (0);
-	else
-		return (1);
+	return (1);
 }
 
 int	ft_find_next_prime(int nb)
 {
-	int	i;
 	int	found;
 
-	i = 0;
 	found = 0;
-	while(found == 0)
+	while (found == 0)
 	{
-		if(ft_is_prime(nb))
+		if (ft_is_prime(nb))
 			found = 1;
-		i++;
 	}
 	return (nb);
 }
